f97c70742a729eb5ad61ff850d9b6e1f4b79c77b
tyreal <995927270@qq.com>
2021-05-10 15:00:00 +0800

    feat: add login到项目当中

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import { ProjectList } from "screens/project-list";
+import { LoginScreen } from "./screens/login";
 
 function App() {
   return (
     <div className="App">
-      <ProjectList />
+      {/*<ProjectList />*/}
+      <LoginScreen />
     </div>
   );
 }
---
+++ src/screens/login/index.tsx
@@ -0,0 +1,38 @@
+import { FormEvent, FormEventHandler } from "react";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const LoginScreen = () => {
+  const login = (param: { username: string; password: string }): void => {
+    fetch(`${apiUrl}/login`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(param),
+    }).then((res) => {});
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const username = (event.currentTarget.elements[0] as HTMLInputElement)
+      .value;
+    const password = (event.currentTarget.elements[1] as HTMLInputElement)
+      .value;
+    login({ username, password });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor={"username"}>用户名</label>
+        <input type={"text"} id={"username"} placeholder={"请输入用户名"} />
+      </div>
+      <div>
+        <label htmlFor={"password"}>密码</label>
+        <input type={"password"} id={"password"} />
+      </div>
+      <button type={"submit"}>登录</button>
+    </form>
+  );
+};
